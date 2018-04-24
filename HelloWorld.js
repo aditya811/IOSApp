c487cd718a884dbd7f5b9497a9849ddb517a87a4
Aditya Shah <ashah8989@gmail.com>
2018-04-23 20:39:33 -0400

    I can push code

---
+++ HelloWorld.js
@@ -0,0 +1,26 @@
+var first_Name = "Adi";
+var last_Name = 'Shah';
+var age = 12;
+var badAss = true;
+var legalDrinkingAge = 21;
+
+
+//console.log(first_Name);
+//console.log(last_Name);
+//console.log(age);
+
+// = - set something to a value
+// == - check that two things are equal
+// === - check that two things are equal value AND they are same type
+// AND conditions
+// OR conditions
+// multiple conditions
+
+
+if(age !== legalDrinkingAge && badAss == true)
+{
+    console.log (first_Name)
+}
+
+
+
